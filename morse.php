c4f10caeabf053a7a290913acfcc16b2623f6676
amanp10 <amanpratik10@gmail.com>
2015-11-08 17:39:55 +0530

    Events 1

--- morse.php
+++ morse.php
@@ -4,7 +4,7 @@
         <section class="container">
             <section class="row" id="logo_menu" role="navigation">                  
                 <div class="col-md-3 col-sm-8 col-xs-2 navbar-brand">
-                    <a class="logo" href=""><i class="ion-ios-home hidden-md hidden-lg"></i><span class="hidden-sm hidden-xs" style="letter-spacing:2px;font-weight:bold;">CODE NICHE</span></a>
+                    <a class="logo" href=""><i class="ion-ios-home hidden-md hidden-lg"></i><span class="hidden-sm hidden-xs" style="letter-spacing:2px;font-weight:bold;">MORSE MYSTERY</span></a>
                 </div>
                 <div class="collapse navbar-collapse" style="padding-top:1em;width:auto;" id="nav_menu">
                 <ul id="nav_menu" class="nav navbar-nav navbar-right" style="overflow-y: visible; overflow-x: visible">                 
@@ -19,10 +19,10 @@
                         <span class="caret"></span>
                         </button>
                         <ul id="drop_ul" class="dropdown-menu" aria-labelledby="dropdownMenu1" style="z-index:100;position:absolute;background:rgba(0,0,0,.7);">
+                        <li><a href="codeniche.php">Code Niche</a></li>
                         <li><a href="envisage.php">Envisage</a></li>
                         <li><a href="idp.php">IDP</a></li>
                         <li><a href="jaribio.php">Jaribio</a></li>
-                        <li><a href="morse.php">Morse Mystery</a></li>
                         <li><a href="ppt.php">Paper Presentation</a></li>
                         <li><a href="recognizance.php">Recognizance</a></li>
                         <li><a href="sos.php">SOS</a></li>
@@ -45,8 +45,9 @@
             <section class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="headings">
-                        <h1 class="wow animated fadeInDown">CODE NICHE</h1>
-                        <p class="wow animated fadeInLeft" style="background:rgba(0,0,0,.7);">In today’s era ….. Those who code ….. Have The Power!!</p>
+                        <h1 class="wow animated fadeInDown">MORSE MYSTERY</h1>
+                        <p class="wow animated fadeInLeft" style="background:rgba(0,0,0,.8);">Have you ever tried to encrypt or decrypt some code? Try decoding this:<br>
+.--. .-. .- ... - ..- - ..</p>
                         <div class="row">
                             <div class="col-xs-6 col-sm-6 col-md-5">
                                 <div style="background:rgba(0,0,0,.7);">
@@ -82,7 +83,7 @@
             
             <section class="row" style="text-align:center;">
                 <p>
-            There is an on-going debate on the extent to which the writing of programs is an art form, a craft, or an engineering discipline. In general, good programming is considered to be the measured application of all three, with the goal of producing an efficient and evolvable software solution. Writing code has a place in the human hierarchy worth somewhere above grave robbing and beneath managing. Today computers can do all sorts of work and making it understand what you intend to do, in a simple concise manner, forms the essence of computer programming. This is done by the efficient programmers who scratch their minds day in and out to make computer follow their commands. In today’s era ….. Those who code ….. Have The Power!! Prastuti'15 brings forth Code Niche, a coding event where you will be required to showcase your talented and creative coding mind and techniques to solve the task at hand. For more information proceed to “Rules and Guidelines” and “Event Structure”.</p>
+                    Morse code is a method of transmitting text information as a series of on-off tones, lights, or clicks that can be directly understood by a skilled listener or observer without special equipment. In 1836, Samuel Morse demonstrated the ability of a telegraph system to transmit information over wires. The information was sent as a series of electrical signals. Short signals are referred to as dits (represented as dots). Long signals are referred to as dahs (represented as dashes). With the advent of radio communications, an international version of Morse code became widely used.</p>
             </section>
             <div class="clearfix"></div>
         </section>
@@ -101,8 +102,17 @@
                 <span class="bb-bottom-right" style="border-color: #fff; "></span>
                 </h1></div>
             </section>
-            <section class="row" style="color:white;text-align:center;background:rgba(0,0,0,.7);">
-                There will be only one round from 9 pm 15th January ,2015 to 2 am 16 th January ,2015 .The coding event will be live on www.codechef.com/CONI2015 . Everyone should have a codechef account to take participate in the event.
+            <section class="row" style="color:white;text-align:center;background:rgba(0,0,0,.7);">PRASTUTI’ 15 introduces a new event to appreciate the importance of this code. The challenge would be to develop their own Telegram Machine working on the principle of MORSE CODE and wireless communications. This machine would include two modules, one for sending message in Morse Code and other for receiving the signals and displaying it in the form of Morse Code on LCD Display.
+                <h2 style="color:white;text-align:center;">Problem Statement</h2>
+                <section class="row" style="color:white;text-align:center;text-align:left;padding:10px;"><ul>
+<li>One participant from each team would be provided with a “Secret Message” on Sending End. This member has to send the message to other team members sitting on the Receiving End.</li>
+<li>Communication between two ends should be done using IR LEDs and TSOP Sensors.</li>
+<li>Sending End should have manual system to send Dots, Dashes and Spaces.</li>
+<li>Receiving End should have a audio/display to display deceived signal.</li>
+<li>Remaining team members have to decode the received signals</li>
+<li>Teams decoding the message correctly with minimum time would be declared as winner.</li>
+</ul>
+            </section>
             </section>
         </section>
     </section>
@@ -120,11 +130,11 @@
                 </h1></div>
             </section>
             <ol>
-                <li>This competition is open only for students who are currently enrolled in any undergraduate course across India.He/She just needs to register on Prastuti's website and follow the event link provided on the event date.</li>
-                <li>Participation is allowed only on individual basis and no team is required.</li>
-                <li>Plagiarism in any form will lead to disqualification.</li>
-                <li>Correct answers will receive full points (no partial credit).</li>
-                <li>Please do not discuss strategy, suggestions or tips in the comments during a live contest.</li>
+                <li>Team strength should not exceed four.</li>
+                <li>Participants have to use INTERNATIONAL MORSE CODE only during the event.</li>
+                <li>Only basic ICs (4xxx and 7xxx) and 8-bit microcontrollers are allowed. Use of any other IC should be intimated to us and verified.</li>
+                <li>Using unfair means would result in disqualification of the team.</li>
+                <li>Judges decision shall be final and binding on all. The organizers reserve the rights to change any or all of the above rules as they deem fit. Change in rules, if any will be highlighted on the website and notified to the registered participants</li>
             </ol>
             <section class="row">
             </section>
--- sos.php
+++ sos.php
@@ -4,7 +4,7 @@
         <section class="container">
             <section class="row" id="logo_menu" role="navigation">                  
                 <div class="col-md-3 col-sm-8 col-xs-2 navbar-brand">
-                    <a class="logo" href=""><i class="ion-ios-home hidden-md hidden-lg"></i><span class="hidden-sm hidden-xs" style="letter-spacing:2px;font-weight:bold;">CODE NICHE</span></a>
+                    <a class="logo" href=""><i class="ion-ios-home hidden-md hidden-lg"></i><span class="hidden-sm hidden-xs" style="letter-spacing:2px;font-weight:bold;">SOS</span></a>
                 </div>
                 <div class="collapse navbar-collapse" style="padding-top:1em;width:auto;" id="nav_menu">
                 <ul id="nav_menu" class="nav navbar-nav navbar-right" style="overflow-y: visible; overflow-x: visible">                 
@@ -19,13 +19,13 @@
                         <span class="caret"></span>
                         </button>
                         <ul id="drop_ul" class="dropdown-menu" aria-labelledby="dropdownMenu1" style="z-index:100;position:absolute;background:rgba(0,0,0,.7);">
+                        <li><a href="codeniche.php">Code Niche</a></li>
                         <li><a href="envisage.php">Envisage</a></li>
                         <li><a href="idp.php">IDP</a></li>
                         <li><a href="jaribio.php">Jaribio</a></li>
                         <li><a href="morse.php">Morse Mystery</a></li>
                         <li><a href="ppt.php">Paper Presentation</a></li>
                         <li><a href="recognizance.php">Recognizance</a></li>
-                        <li><a href="sos.php">SOS</a></li>
                         <li><a href="index.php">HOME</a></li>
                         </ul>
                         </div>
@@ -45,8 +45,8 @@
             <section class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="headings">
-                        <h1 class="wow animated fadeInDown">CODE NICHE</h1>
-                        <p class="wow animated fadeInLeft" style="background:rgba(0,0,0,.7);">In today’s era ….. Those who code ….. Have The Power!!</p>
+                        <h1 class="wow animated fadeInDown">Solution On Spot</h1>
+                        <p class="wow animated fadeInLeft" style="background:rgba(0,0,0,.7);">Technology has advanced at startling rates, in the era of technological development speed is the need of the hour</p>
                         <div class="row">
                             <div class="col-xs-6 col-sm-6 col-md-5">
                                 <div style="background:rgba(0,0,0,.7);">
@@ -82,7 +82,7 @@
             
             <section class="row" style="text-align:center;">
                 <p>
-            There is an on-going debate on the extent to which the writing of programs is an art form, a craft, or an engineering discipline. In general, good programming is considered to be the measured application of all three, with the goal of producing an efficient and evolvable software solution. Writing code has a place in the human hierarchy worth somewhere above grave robbing and beneath managing. Today computers can do all sorts of work and making it understand what you intend to do, in a simple concise manner, forms the essence of computer programming. This is done by the efficient programmers who scratch their minds day in and out to make computer follow their commands. In today’s era ….. Those who code ….. Have The Power!! Prastuti'15 brings forth Code Niche, a coding event where you will be required to showcase your talented and creative coding mind and techniques to solve the task at hand. For more information proceed to “Rules and Guidelines” and “Event Structure”.</p>
+            We are all strangers to our hidden potential until we confront problems that reveal our capabilities. Prastuti is a long held tradition at IIT-BHU. Started at the dawn of the new millennium in 2001, the emphasis of Prastuti has always been on "Technology in the Service of Mankind". Today, technology has revolutionized all aspects of traditional life in the world. A decade ago, we had to stand in endless queues to make a simple phone call. Today India has more than 950 million mobile connections that have enabled us to make video calls through handheld devices (complex enough to run an entire satellite) at uber-fast speed. These pivotal technologies have developed at startling rates. In the era of technological development speed is the need of the hour. Yet in this race for faster, stronger, better technologies we are unable to impart the students the importance of reliability, consistency of the solution. In most of the cases the solutions falter in incorporating these characteristics in them.</p>
             </section>
             <div class="clearfix"></div>
         </section>
@@ -101,8 +101,7 @@
                 <span class="bb-bottom-right" style="border-color: #fff; "></span>
                 </h1></div>
             </section>
-            <section class="row" style="color:white;text-align:center;background:rgba(0,0,0,.7);">
-                There will be only one round from 9 pm 15th January ,2015 to 2 am 16 th January ,2015 .The coding event will be live on www.codechef.com/CONI2015 . Everyone should have a codechef account to take participate in the event.
+            <section class="row" style="color:white;text-align:center;background:rgba(0,0,0,.7);">In the spirit of this speed-with-consistency idea, SOLUTION-ON-SPOT(S.O.S.) has been designed to empower the fast thought-out solutions with efficiency. S.O.S. is a platform for tomorrow's tech-leaders to bring out the 'Einstein' in them. The main aim of this event is to come up with efficient solutions for a given real life problem and all this while the 'clock is ticking'. This event will help the candidates in understanding the intricate and pivotal role technology plays in the real-life. In this event the participants have to be well-equipped in their technical know-how & also should be able to come up with an effective solution. All this should be accomplished while racing against the clock.
             </section>
         </section>
     </section>
@@ -120,11 +119,10 @@
                 </h1></div>
             </section>
             <ol>
-                <li>This competition is open only for students who are currently enrolled in any undergraduate course across India.He/She just needs to register on Prastuti's website and follow the event link provided on the event date.</li>
-                <li>Participation is allowed only on individual basis and no team is required.</li>
-                <li>Plagiarism in any form will lead to disqualification.</li>
-                <li>Correct answers will receive full points (no partial credit).</li>
-                <li>Please do not discuss strategy, suggestions or tips in the comments during a live contest.</li>
+                <li>Participating teams will have to register themselves through the online form made available on the Prastuti website. The teams arriving from other colleges will also have the option of on-the-spot registration.</li>
+                <li>The teams can have a max. of 4 members. Teams can consist members from different colleges.</li>
+                <li>Groups will be provided a fixed number of cases/scenarios. The scenarios will be provided one after another and there will be a fixed time limit for solving each question. They will have to propose a rough solution of the cases within the time. The time limit will be decided according to the difficulty level of the particular question.The professor will select the qualifying solutions for every question. For every question attempted, the teams will receive points & for every question selected for the next round the corresponding team will receive some more points. Ultimatum Round</li>
+                <li>The teams shortlisted based on their performance in the first round shall be called for the ultimatum round. They will be informed a day prior to this round. If for a team multiple solutions are selected, the team will have the choice of choosing the final solutions (for a maximum of 2 cases) that they will present.The teams will have to present a complete solution in a detailed presentation for where they shall be judged by a panel of professors. Points will be given for the teams for their final presentation.</li>
             </ol>
             <section class="row">
             </section>
